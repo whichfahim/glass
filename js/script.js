087c9e277c957ea2a31ab218e73714df2d97c9e1
whichFahim <31901239+whichFahim@users.noreply.github.com>
2021-09-26 13:59:21 +0600

    Sticky navigation
    Made the navigation sticky using jQuery, added some functionality to the nav element.

---
+++ js/script.js
@@ -0,0 +1,54 @@
+$(document).ready(function() {
+    
+    /* STICKY NAVIGATION */
+    $('.js--section-about').waypoint(function(direction) {
+        if (direction == "down") {
+            $('nav').addClass('sticky');
+        } else {
+            $('nav').removeClass('sticky');
+        }
+        
+    });
+
+    /* SCROLL ON BUTTONS */
+    $('.js--scroll-to-products').click(function() {
+        $('html, body').animate({scrollTop: $('.js--section-products').offset().top}, 1000);
+    });
+
+    /*
+    $('.js--scroll-to-stories').click(function() {
+        $('html, body').animate({scrollTop: $('.js--section-stories').offset().top}, 1000);
+    });
+    */
+
+    /* NAVIGATION SCROLL */
+    
+});
+
+/*
+    var waypoints = $('#handler-first').waypoint(function(direction) {
+        notify(this.element.id + ' hit 25% from top of window') 
+      },{
+        offset: '25%'
+    })
+
+    // Scroll to specific values
+    // scrollTo is the same
+    window.scroll({
+    top: 2500, 
+    left: 0, 
+    behavior: 'smooth'
+    });
+
+    // Scroll certain amounts from current position 
+    window.scrollBy({ 
+    top: 100, // could be negative value
+    left: 0, 
+    behavior: 'smooth' 
+    });
+
+    // Scroll to a certain element
+    document.querySelector('.hello').scrollIntoView({ 
+    behavior: 'smooth' 
+    });
+    */
